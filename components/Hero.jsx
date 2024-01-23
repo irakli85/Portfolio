65c7b2578395aee8247c46e616a5f50dfae199a0
irakli <iberidze38@gmail.com>
2024-01-24 00:46:19 +0400

    first commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -34,7 +34,10 @@ const Hero = () => {
                     <Socials containerStyles='flex gap-x-6 mx-auto xl:mx-0' iconsStyles='text-foreground text-[22px] hover:text-primary transition-all'/>
                 </div>
                  {/* image */}
-                 <div className="hidden xl:flex relative">image</div>
+                 <div className="hidden xl:flex relative">
+                    <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+                    <DevImg containerStyles='bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom' imgSrc='/hero/developer.png'/>
+                 </div>
                  {/* icon */}
                  <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
                     <RiArrowDownSLine className="text-3xl text-primary"/>
