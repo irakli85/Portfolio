81b5017dfdd4d06dbb09fa7af0ce7d194c522879
irakli <iberidze38@gmail.com>
2024-01-26 18:11:44 +0400

    first commit

--- components/About.jsx
+++ components/About.jsx
@@ -56,17 +56,17 @@ const qualificationData = [
         data: [
             {
                 company: 'ABC Inc.',
-                qualification: 'Softwear Engineer',
+                role: 'Softwear Engineer',
                 years: '2018-2020'
             },
             {
                 company: 'X corporation',
-                qualification: 'Senior Developer',
+                role: 'Senior Developer',
                 years: '2020-2022'
             },
             {
                 company: 'Tech Inovators',
-                qualification: 'Lead Developer',
+                role: 'Lead Developer',
                 years: '2022-Present'
             }
         ]
@@ -136,10 +136,62 @@ const About = () => {
                         <div className="text-lg mt-12 xl:mt-8">
                             {/* personal */}
                             <TabsContent value='personal'>
-                                personal info
+                                <div className="text-center xl:text-left">
+                                    <h3 className="h3 mb-4">Unmatched Service Quality for Over 10 Years</h3>
+                                    <p className="subtitle max-w-xl mx-auto xl:mx-0">I specialize in crafting intuitive websites with cutting-edge technology, delivering dynamic and engaging user experiences.</p>
+                                    {/* icons */}
+                                    <div className="grid xl:grid-cols-2 gap-4 mb-12">
+                                        {
+                                            infoData.map( (item, index) => {
+                                                return(
+                                                    <div key={index} className="flex items-center gap-x-4 mx-auto xl:mx-0">
+                                                        <div className="text-primary">{item.icon}</div>
+                                                        <div>{item.text}</div>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    {/* languages */}
+                                    <div className="flex flex-col gap-y-2">
+                                        <div className="text-primary">Language Skill</div>
+                                        <div className="border-b border-border"></div>
+                                        <div>English, Russian, Georgian</div>
+                                    </div>
+                                </div>
                             </TabsContent>
+                            {/* qualifications */}
                             <TabsContent value='qualifications'>
-                                qualifications
+                                <div>
+                                    <h3 className="h3 mb-8 text-center xl:text-left">My Awesome Journey</h3>
+                                    {/* experience & education wrapper */}
+                                    <div>
+                                        {/* experience */}
+                                        <div>
+                                            <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                                                <Briefcase/>
+                                                <h4 className="capitalize font-medium">
+                                                    {getData(qualificationData, 'experience').title}
+                                                </h4>
+                                            </div>
+                                            {/* list */}
+                                            <div>
+                                                {getData(qualificationData, 'experience').data.map( (item, index) => {
+                                                    const {company, role, years} = item
+                                                    return(
+                                                        <div key={index}>
+                                                           <div>{company}</div>
+                                                           <div>{role}</div>
+                                                           <div>{years}</div>
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
+                                        {/* education */}
+                                        <div>education</div>
+                                    </div>
+                                </div>
                             </TabsContent>
                             <TabsContent value='skills'>
                                 skills
