60c9bd3039b8f36d15d84bc91069170b22a997ef
irakli <iberidze38@gmail.com>
2024-01-30 09:19:41 +0400

    skills & tools

--- components/About.jsx
+++ components/About.jsx
@@ -226,9 +226,37 @@ const About = () => {
                             {/* skills */}
                             <TabsContent value='skills'>
                                 <div className="text-center xl:text-left">
-                                    <h3 className="h3 mb-4">Tools I Use Everyday</h3>
+                                    <h3 className="h3 mb-4">What I Use Everyday</h3>
+                                    <div className="mb-16">
+                                        <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                                        <div className="border-b border-border mb-4"></div>
+                                        {/* skill list */}
+                                        <div>
+                                            {getData(skillData, 'skills').data.map( (item, index) => {
+                                                const {name} = item;
+                                                return (
+                                                    <div key={index} className="w-2/4 text-center xl:text-left mx-auto xl:mx-0">
+                                                        <div className="font-medium">{name}</div>
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                    {/* tools */}
                                     <div>
-                                        <h4>Skills</h4>
+                                        <h4 className="text-xl font-semibold mb-2 xl:text-left">Tools</h4>
+                                        <div className="border-b border-border mb-4"></div>
+                                        {/* tool list */}
+                                        <div className="flex gap-x-8 justify-center xl:justify-start">
+                                            {getData(skillData, 'tools').data.map( (item, index) => {
+                                                const {imgPath} = item
+                                                return(
+                                                    <div key={index}>
+                                                        <Image src={imgPath} width={48} height={48} alt="" priority/>
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
                                     </div>
                                 </div>
                             </TabsContent>
